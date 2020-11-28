7d01af2c57b48da4beb1f9ca29e1391cb80e0e3b
hiteshi15 <74048103+hiteshi15@users.noreply.github.com>
2020-11-28 20:12:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+const Engine = Matter.Engine; 
+const World = Matter.World; 
+const Bodies = Matter.Bodies; 
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+function setup(){
+  createCanvas(900,400); 
+  engine = Engine.create();
+  world = engine.world; 
+  Engine.run(engine);
+
+  //level two 
+  block8 = new Block(330,235,30,40); 
+  block9 = new Block(360,235,30,40); 
+  block10 = new Block(390,235,30,40); 
+  block11 = new Block(420,235,30,40); 
+  block12 = new Block(450,235,30,40); 
+  //level three 
+  block13 = new Block(360,195,30,40); 
+  block14 = new Block(390,195,30,40); 
+  block15 = new Block(420,195,30,40); 
+  //top 
+  block16 = new Block(390,155,30,40);
+
+  ground = new Ground(330,225,170,10);
+  // creating ground, sling, polygon;
+
+}
+
+function draw(){
+
+}
+
+function display(){
+
+}
